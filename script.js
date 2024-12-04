01fa4211924b97436b40390db0b66863e3c9bb0a
Abubakar <shaikhabubakar75@gmail.com>
2024-12-05 00:20:23 +0530

    add randome generate number code in script with branch contact

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ document.getElementById('play-button').addEventListener('click', () => {
 document.getElementsByClassName('menu-button').addEventListener('click', () => {
   alert('Enter Setting Mode');
 })
+const body = document.getElementsByTagName('body')[0]
+
+const p = document.createElement('p')
+const btn = document.createElement('button')
+btn.textContent = "Generate Random Number";
+
+function generateRandomeNumber() {
+  p.innerHTML = Math.floor(Math.random())
+}
+btn.addEventListener('click', () => {
+  generateRandomeNumber()
+})
+
+body.appendChild(p)
+body.appendChild(btn)
