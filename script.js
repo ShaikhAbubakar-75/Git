9c7853f1a37a71c6cbff4bdd227ff003f1039027
Abubakar <shaikhabubakar75@gmail.com>
2024-12-05 00:29:40 +0530

    remove eventlistner code to setting menu

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 document.getElementById('play-button').addEventListener('click', () => {
   confirm('Starting the game...');
 });
-document.getElementsByClassName('menu-button').addEventListener('click', () => {
-  alert('Enter Setting Mode');
-})
 
 
 
